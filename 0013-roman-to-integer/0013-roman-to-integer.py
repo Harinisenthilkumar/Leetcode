46f421b9a8cda4f476bd0cc28bf11607cae8f7a0
Harini <88531739+Harinisenthilkumar@users.noreply.github.com>
2024-10-08 22:12:06 +0530

    Time: 54 ms (17.21%), Space: 16.6 MB (12.7%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Mapping of Roman numerals to their integer values
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If the current value is less than the next value, subtract it
+            if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                total -= roman_to_int[s[i]]
+            else:
+                total += roman_to_int[s[i]]
+        
+        return total
+
+# Example usage:
+solution = Solution()
+print(solution.romanToInt("III"))      # Output: 3
+print(solution.romanToInt("LVIII"))    # Output: 58
+print(solution.romanToInt("MCMXCIV"))  # Output: 1994
